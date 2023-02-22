62be69dcf910ff03afddd6a65b4a8c432bb304f4
Sean Coutinho <SeanCoutinho@my.uopeople.edu>
2023-02-22 23:22:43 +0200

    total amount for expenses logic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import { nanoid } from 'nanoid';
+import ExpenseList from "./components/ExpenseList"
+import Alert from "./components/Alert"
+import ExpenseForm from './components/ExpenseForm';
 
 function App() {
+
+  const [expenses, setExpenses] = useState([
+    {id: nanoid(), charge: "rent", amount: 1600},
+    {id: nanoid(), charge: "car payment", amount: 2000},
+    {id: nanoid(), charge: "credit card bill", amount: 1200}
+  ])
+  
+  console.log(expenses, setExpenses);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Alert />
+      <h1>Budget Calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses}/>
+      </main>
+      <h1>Total Spending: <span className="total">
+        $ {expenses.reduce((acc, curr) => {
+          return acc+=curr.amount
+        }, 0)}</span></h1>
+    </>
+  )
 }
 
 export default App;
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Item from "./ExpenseItem"
+
+export default function ExpenseList() {
+  return (
+    <div>
+      Expense List
+      <Item />
+    </div>
+  )
+}
